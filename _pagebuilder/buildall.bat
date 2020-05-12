2741ce8f59b6911a6c3c4707b1f5cddec5d82fe8
Sharlikran <danielhmpdx16@gmail.com>
2020-05-11 19:53:49 -0700

    opps named then wrong

--- _pagebuilder/buildall.bat
+++ _pagebuilder/buildall.bat
@@ -1,7 +1,7 @@
 C:\Python38-32\python.exe wtxt-wiki.py genHtml index.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml houses.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml 1-install_luarocks.txt
-C:\Python38-32\python.exe wtxt-wiki.py genHtml 2-setup-intellij..txt
+C:\Python38-32\python.exe wtxt-wiki.py genHtml 2-setup-intellij.txt
 REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 3-wryemashusage.txt
 REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 4-generalusage.txt
 REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 5-advancedusage.txt

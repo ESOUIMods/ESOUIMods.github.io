c6c6fb1ea39dbc7d87091150445d4ffb4659d3b9
Sharlikran <danielhmpdx16@gmail.com>
2022-02-01 17:04:35 -0800

    Fixed typo 100 vs 1000

--- _pagebuilder/buildall.bat
+++ _pagebuilder/buildall.bat
@@ -4,4 +4,3 @@ C:\Python38-32\python.exe wtxt-wiki.py genHtml 1-install_luarocks.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml 2-setup_intellij.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml 3-master_merchant.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml 4-french.txt
-pause
